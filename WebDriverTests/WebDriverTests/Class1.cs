6940858b12aa97098f59423d6493d0a15755e6da
Malgorzata Sanak-Jonczyk <MSJK@simcorp.com>
2017-05-24 11:00:10 +0200

    update

--- WebDriverTests/WebDriverTests/Class1.cs
+++ WebDriverTests/WebDriverTests/Class1.cs
@@ -30,9 +30,15 @@ namespace WebDriverTests
         public void FixMe() //zeby test dzialal musi byc zawsze public void z atrybutem Fact
         {
             _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
-            IWebElement firstPost = _driver.FindElementByClassName("post-title"); //IWebElement to konkretny rodzaj zmeinnej, mozna bylo var
-            String firstNoteTitle= firstPost.FindElement(By.TagName("a")).Text;
-                //String to konkretny rodzaj zmeinnej, mozna bylo var
+            // IWebElement firstPost = _driver.FindElementByClassName("post-title"); //IWebElement to konkretny rodzaj zmeinnej, mozna bylo var
+            // String firstNoteTitle= firstPost.FindElement(By.TagName("a")).Text;   //String to konkretny rodzaj zmeinnej, mozna bylo var
+            // jezeli notatka jest druga na blodu mozna to tez XPathem:(//header[@class = "post-title"][2]//a)[1] - znajdz pierwszy element o nazwie klasy...i tam drugi eleent dla ankora 'a'
+
+
+
+            var posts = _driver.FindElementsByClassName("post-title"); //zamiast var mozna ReadOnlyCollection<IWedElement> - czyli kolekcje elementow typu IWeb
+            var secondPost = posts[1];                                 //wtedy tutja tez zamiast var to IWebElement
+            string firstNoteTitle = secondPost.FindElement(By.TagName("a")).Text;
 
             Assert.Equal("Vivamus aliquam feugiat", firstNoteTitle);
         }
